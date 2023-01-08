37239dc2b9f49755baa2a8095c54412184a06242
Henrique523 <guilherme.hlg.eng@gmail.com>
2023-01-08 19:57:14 -0300

    inicia capitulo notificando o usuario

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     </div>
 
     <div class="column is-three-quarter conteudo">
+      <Notificacoes />
       <router-view></router-view>
     </div>
   </main>
@@ -14,10 +15,11 @@
 
 import {defineComponent} from 'vue';
 import BarraLateral from "@/components/BarraLateral.vue";
+import Notificacoes from "@/components/Notificacoes.vue";
 
 export default defineComponent({
   name: 'App',
-  components: {BarraLateral},
+  components: {Notificacoes, BarraLateral},
 
   data: () => ({
     modoEscuroAtivo: false,
